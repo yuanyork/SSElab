5b872720f25e8ad19f1229d8a95490585296c5ac
yuanyork <1055286585@qq.com>
2022-04-01 21:11:18 +0800

    v0.2

---
+++ linktable.go
@@ -0,0 +1,92 @@
+package main
+
+import "sync"
+
+type LinkTableNode struct {
+	pNext *LinkTableNode
+}
+
+type LinkTable struct {
+	pHead     *LinkTableNode
+	pTail     *LinkTableNode
+	SumOfNode int
+	mutex     *sync.Mutex
+}
+
+type contrl interface {
+	CreateLinktable() *LinkTable
+	DeleteLinktable() bool
+	AddLinktableNode(*LinkTableNode) bool
+	DeleteLinktableNode(*LinkTableNode) bool
+	GetLinktableHead() *LinkTableNode
+	GetNextLinktableNode(*LinkTableNode) bool
+}
+
+func (l *LinkTable) AddLinktableNode(lNode *LinkTableNode) bool {
+	if lNode == nil {
+		return false
+	}
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+	l.SumOfNode++
+	if l.pHead == nil {
+		l.pHead = lNode
+	}
+	if l.pTail == nil {
+		l.pTail = lNode
+	} else {
+		l.pTail.pNext = lNode
+		l.pTail = lNode
+	}
+	return true
+}
+
+func (l *LinkTable) DeleteLinktableNode(lNode *LinkTableNode) bool {
+	if lNode == nil || l.pHead == nil {
+		return false
+	}
+	l.mutex.Lock()
+	defer func() {
+		if l.SumOfNode == 0 {
+			l.pTail = nil
+		}
+		l.mutex.Unlock()
+	}()
+	tmp := l.pHead
+	l.SumOfNode--
+	for tmp.pNext != nil {
+		if lNode == l.pHead {
+			l.pHead = l.pHead.pNext
+			return true
+		}
+		if tmp.pNext == lNode {
+			if lNode == l.pTail && tmp.pNext == l.pTail {
+				l.pTail = tmp
+				l.pTail = nil
+				return true
+			}
+			tmp.pNext = tmp.pNext.pNext
+			return true
+		}
+		tmp = tmp.pNext
+	}
+	return false
+}
+
+func (l *LinkTable) GetLinktableHead() *LinkTableNode {
+	return l.pHead
+}
+
+func (l *LinkTable) GetNextLinktableNode(pNode *LinkTableNode) *LinkTableNode {
+	if pNode == nil {
+		return nil
+	}
+	pTemp := l.pHead
+	for pTemp != nil {
+		if pTemp == pNode {
+			return pTemp.pNext
+		}
+		pTemp = pTemp.pNext
+	}
+	return nil
+}
--- menu.go
+++ menu.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"sync"
+	"unsafe"
 )
 
 const (
@@ -13,51 +15,90 @@ const (
 )
 
 type DataNode struct {
+	next    LinkTableNode
 	cmd     string
 	desc    string
 	handler func() int
-	next    *DataNode
 }
 
-var head []DataNode = []DataNode{
-	DataNode{"help", "this is help cmd!", help, nil},
-	DataNode{"version", "menu program v1.0", nil, nil},
-	DataNode{"quit", "Quit from menu", Quit, nil},
+type pDataNode *DataNode
+
+var pLinktbl *LinkTable
+
+func InitMenuData() {
+	pNode := &DataNode{
+		cmd:     "help",
+		desc:    "this is help cmd!",
+		handler: help,
+	}
+	pLinktbl.AddLinktableNode((*LinkTableNode)(unsafe.Pointer(pNode)))
+	pNode = &DataNode{
+		cmd:     "version",
+		desc:    "menu program v1.0",
+		handler: nil,
+	}
+	pLinktbl.AddLinktableNode((*LinkTableNode)(unsafe.Pointer(pNode)))
+	pNode = &DataNode{
+		cmd:     "quit",
+		desc:    "Quit from menu",
+		handler: Quit,
+	}
+	pLinktbl.AddLinktableNode((*LinkTableNode)(unsafe.Pointer(pNode)))
+}
+
+// var head []DataNode = []DataNode{
+// 	DataNode{"help", "this is help cmd!", help, nil},
+// 	DataNode{"version", "menu program v1.0", nil, nil},
+// 	DataNode{"quit", "Quit from menu", Quit, nil},
+// }
+
+func FindCmd(cmd string) pDataNode {
+	p := (pDataNode)(unsafe.Pointer(pLinktbl.GetLinktableHead()))
+	if p == nil {
+		return nil
+	}
+	for p != nil {
+		if strings.Compare(p.cmd, cmd) == 0 {
+			return p
+		}
+		p = (pDataNode)(unsafe.Pointer(pLinktbl.GetNextLinktableNode((*LinkTableNode)(unsafe.Pointer(p)))))
+	}
+	return nil
 }
 
 func main() {
 	/* cmd line begins */
-	head[0].handler = help
-	head[0].next = &head[1]
-	head[1].next = &head[2]
+	pLinktbl = &LinkTable{
+		pHead:     nil,
+		pTail:     nil,
+		SumOfNode: 0,
+		mutex:     &sync.Mutex{},
+	}
+	InitMenuData()
 	for {
 		var cmd string
-		fmt.Printf("Input a cmd name > ")
-		fmt.Scanf("%s", &cmd)
-		var p *DataNode = &head[0]
-		for p != nil {
-
-			if strings.Compare(p.cmd, cmd) == 0 {
-				fmt.Printf("%s - %s\n", p.cmd, p.desc)
-				if p.handler != nil {
-					p.handler()
-				}
-				break
+		for {
+			fmt.Print("Input a cmd name > ")
+			fmt.Scanf("%s", &cmd)
+			p := FindCmd(cmd)
+			if p == nil {
+				fmt.Printf("This is a wrong cmd!\n")
+				continue
 			}
-			p = p.next
-		}
-		if p == nil {
-			fmt.Printf("This is a wrong cmd!\n ")
+			fmt.Printf("%s - %s\n", p.cmd, p.desc)
+			if p.handler != nil {
+				p.handler()
+			}
 		}
 	}
 }
 
 func help() int {
 	fmt.Printf("Menu List:\n")
-	var p *DataNode = (*DataNode)head
+	var p *DataNode = (*DataNode)(unsafe.Pointer(pLinktbl.GetLinktableHead()))
 	for p != nil {
 		fmt.Printf("%s - %s\n", p.cmd, p.desc)
-		p = p.next
+		p = (*DataNode)(unsafe.Pointer(pLinktbl.GetNextLinktableNode((*LinkTableNode)(unsafe.Pointer(p)))))
 	}
 	return 0
 }
